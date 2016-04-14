83f9c675b79306bb45e9d29fe04212984499a350
Kiran Kumar Mohanty <kiran.mohanty1992@gmail.com>
2016-04-14 19:03:02 +0530

    hnf

--- AngularJSAuthentication/AngularJSAuthentication.API/Repository/AuthRepository.cs
+++ AngularJSAuthentication/AngularJSAuthentication.API/Repository/AuthRepository.cs
@@ -5,11 +5,12 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace AngularJSAuthentication.API.Repository
 {
-    public class AuthRepository:IDisposable
+    public class AuthRepository : IDisposable
     {
         private AuthContext _ctx;
         private UserManager<IdentityUser> _userManager;
@@ -18,6 +19,31 @@ namespace AngularJSAuthentication.API.Repository
         {
             _ctx = new AuthContext();
             _userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(_ctx));
+        }
+
+        public async Task<IdentityResult> RegisterUser(UserModel userModel)
+        {
+            IdentityUser user = new IdentityUser
+            {
+                UserName = userModel.UserName
+            };
+
+            var result = await _userManager.CreateAsync(user, userModel.Password);
+            return result;
+        }
+
+        public async Task<IdentityUser> FindUser(string userName, string password)
+        {
+            IdentityUser user = await _userManager.FindAsync(userName, password);
+            return user;
+        }
+
+        public void Dispose()
+        {
+            if (_ctx != null)
+                _ctx.Dispose();
+            if (_userManager != null)
+                _userManager.Dispose();
         }
     }
 }
--- AngularJSAuthentication/AngularJSAuthentication.API/Startup.cs
+++ AngularJSAuthentication/AngularJSAuthentication.API/Startup.cs
@@ -3,6 +3,9 @@ using System.Threading.Tasks;
 using Microsoft.Owin;
 using Owin;
 using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using AngularJSAuthentication.API.Providers;
+using Microsoft.Owin.Cors;
 
 [assembly: OwinStartup(typeof(AngularJSAuthentication.API.Startup))]
 
@@ -13,8 +16,26 @@ namespace AngularJSAuthentication.API
         public void Configuration(IAppBuilder app)
         {
             HttpConfiguration config = new HttpConfiguration();
+
+            ConfigureOAuth(app);
+
             WebApiConfig.Register(config);
+            app.UseCors(CorsOptions.AllowAll);
             app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
         }
     }
 }
