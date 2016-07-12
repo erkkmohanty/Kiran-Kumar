335adb60fe6c4a86c6c02f6ac133d7ae9ba936b0
Kiran Kumar Mohanty <kiran.mohanty1992@gmail.com>
2016-07-12 16:00:16 +0530

    Commit

---
+++ Angular JS/Angular_SPA/ASP.Net With Angular Unit Test.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ASP.Net_With_Angular_Unit_Test.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
