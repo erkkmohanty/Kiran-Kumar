1dd0ace2f099dcfdd107a2246d51d19beb5ffc59
Kiran Kumar Mohanty <kiran.mohanty1992@gmail.com>
2016-08-18 10:16:10 +0530

    Anggular JS Authentication Old
    Anggular JS Authentication Old

---
+++ AngularJSAuthentication/AngularJSAuthentication Old/AngularJSAuthentication.API/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using AngularJSAuthentication.API.Old.Providers;
+using Microsoft.Owin.Cors;
+
+[assembly: OwinStartup(typeof(AngularJSAuthentication.API.Old.Startup))]
+
+namespace AngularJSAuthentication.API.Old
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            ConfigureOAuth(app);
+
+            WebApiConfig.Register(config);
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
