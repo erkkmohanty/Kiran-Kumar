978af78ea9311ebfd74e65bf3cd8b724b45e5341
Kiran Kumar Mohanty <kiran.mohanty1992@gmail.com>
2016-08-29 18:01:13 +0530

    the end

--- Angular JS/Angular_SPA/Angular User Registration With Login/register/register.controller.js
+++ Angular JS/Angular_SPA/Angular User Registration With Login/register/register.controller.js
@@ -1,16 +1,30 @@
-﻿(function() {
+﻿(function () {
     angular.module("app")
         .controller("RegisterController", RegisterController);
-    RegisterController.$inject = ["UserService", "$location", "$rootScope", "FlashService"];
+    RegisterController.$inject = ["UserService", "$location", "$rootScope", "FlashService", "$timeout"];
 
-    function RegisterController(UserService, $location, $rootScope, FlashService) {
+    function RegisterController(UserService, $location, $rootScope, FlashService, $timeout) {
         debugger;
         var vm = this;
+        vm.countryList = [{ Id: 1, Name: "India" }, { Id: 2, Name: "Australia" }, { Id: 3, Name: "US" }, { Id: 4, Name: "UK" }];
+        vm.selectedCountry = null;
+        vm.stateList = [];
+        vm.selectedState = null;
         vm.register = register;
+        vm.getStates = getStates
+        function getStates() {
+            sleep(500).then(() => {
+                debugger;
+                // Do something after the sleep!
+                console.log(vm.selectedCountry);
+                vm.stateList = [{ Id: 1, Name: "Odisha" }, { Id: 2, Name: "Delhi" }, { Id: 3, Name: "W.B." },
+                    { Id: 4, Name: "Tripura" }, { Id: 5, Name: "U.P." }];
+            });
+        }
         function register() {
             vm.dataLoading = true;
             UserService.Create(vm.user)
-                .then(function(response) {
+                .then(function (response) {
                     if (response.success) {
                         FlashService.Success("Registration Successfull", true);
                         $location.path("/login");
@@ -19,6 +33,10 @@
                         vm.dataLoading = false;
                     }
                 });
+        }
+
+        function sleep(time) {
+            return new Promise((resolve) => $timeout(resolve, time));
         }
     }
 })();
