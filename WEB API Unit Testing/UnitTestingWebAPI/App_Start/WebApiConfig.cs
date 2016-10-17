6ecce954b276feb136d285d0287981b65776f3ff
Kiran Kumar Mohanty <kiran.mohanty1992@gmail.com>
2016-10-17 09:58:34 +0530

    cmmit

--- WEB API Unit Testing/UnitTestingWebAPI/App_Start/WebApiConfig.cs
+++ WEB API Unit Testing/UnitTestingWebAPI/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Routing;
 
 namespace UnitTestingWebAPI
 {
@@ -13,7 +14,6 @@ namespace UnitTestingWebAPI
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
