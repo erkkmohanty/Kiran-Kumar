885c5eda52ed7b77864e4ad2649be6885a55a24f
Kiran Kumar Mohanty <kiran.mohanty1992@gmail.com>
2017-02-11 18:16:34 +0530

    Generic Binary Tree created

--- DSA/DSA/Program.cs
+++ DSA/DSA/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DSA.Tree;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,10 @@ namespace DSA
 {
     class Program
     {
+        static int[] data = { 10, 34, 45, 23, 55, 1, 2, 3, 4, 90 };
         static void Main(string[] args)
         {
-            UtilityManager.CreateLinkedList();
+            //UtilityManager.CreateLinkedList();
             //UtilityManager.Count();
 
             //WordCounter.CountWord("Kiran Kumar Kira Mohanty Kiran K iran KKiran","Kiran");
@@ -19,15 +21,30 @@ namespace DSA
             //MaximumSubArray.FindMaxSubArray(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 });
             //MaximumSubArray.FindMaxSubArrayWithIndices(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 });
             //Test();
+            TestBinaryTree();
+            Console.ReadLine();
         }
 
         static void Test()
         {
             int a = 17;
             a--;
-            a =a- ++a+a--+a++;
+            a = a - ++a + a-- + a++;
             Console.WriteLine(a);
             Console.ReadLine();
+        }
+
+        static void TestBinaryTree()
+        {
+             BinaryTree<int> bTree= new BinaryTree<int>(67);
+            data.ToList().ForEach(el => bTree.InsertData(el));
+            Console.WriteLine("Pre Order");
+            bTree.PreOrderTraverse(bTree.Head);
+            Console.WriteLine("In Order");
+            bTree.InOrderTraverse(bTree.Head);
+            Console.WriteLine("Post Order");
+            bTree.PostOrderTraverse(bTree.Head);
+
         }
     }
 }
---
+++ DSA/DSA/Tree/BinaryTree.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Tree
+{
+    public class BinaryTree<T> where T : struct, IComparable
+    {
+        public BNode<T> Head { get; set; }
+        public static bool IsGreaterThan(T value, T other)
+        {
+            return value.CompareTo(other) > 0;
+        }
+        public static bool IsGreaterThanEqual(T value, T other)
+        {
+            return value.CompareTo(other) >= 0;
+        }
+
+        public static bool IsLessThan(T value, T other)
+        {
+            return value.CompareTo(other) < 0;
+        }
+        public static bool IsLessThanEqual(T value, T other)
+        {
+            return value.CompareTo(other) <= 0;
+        }
+
+        public BinaryTree(T data)
+        {
+            Head = new BNode<T>(data);
+        }
+        /*We can traverse the BinaryTree in three ways
+         * 1.PreOrder-VLR(Visit,Left,Right)
+         * 2.InOrder-LVR(Left,Visit,Right)
+         * 3.PostOrder-LRV(Left,Right,Visit)
+         */
+        public void PreOrderTraverse(BNode<T> bNode)
+        {
+            if (bNode == null)
+                return;
+            Console.WriteLine(bNode.Data);
+            PreOrderTraverse(bNode.Left);
+            PreOrderTraverse(bNode.Right);
+        }
+        public void InOrderTraverse(BNode<T> bNode)
+        {
+            if (bNode == null)
+                return;
+            InOrderTraverse(bNode.Left);
+            Console.WriteLine(bNode.Data);
+            InOrderTraverse(bNode.Right);
+        }
+        public void PostOrderTraverse(BNode<T> bNode)
+        {
+            if (bNode == null)
+                return;
+            PostOrderTraverse(bNode.Left);
+            PostOrderTraverse(bNode.Right);
+            Console.WriteLine(bNode.Data);
+        }
+
+        public BNode<T> InsertData(T data)
+        {
+            BNode<T> node = new BNode<T>(data);
+            if (Head == null)
+                return Head = node;
+            BNode<T> parentNode = null, currentNode = Head;
+            while (currentNode != null)
+            {
+                parentNode = currentNode;
+                if (IsLessThanEqual(currentNode.Data,data))
+                    currentNode = currentNode.Right;
+                else
+                    currentNode = currentNode.Left;
+            }
+            if ((IsLessThanEqual(parentNode.Data,data)))
+                parentNode.Right = node;
+            else
+                parentNode.Left = node;
+            return parentNode;
+
+        }
+    }
+}
