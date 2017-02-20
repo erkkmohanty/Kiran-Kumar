9ea023e67cc6222680dd4d91e41fb6d740ee5155
Kiran Kumar Mohanty <kiran.mohanty1992@gmail.com>
2017-02-20 10:34:23 +0530

    5tttttttttttt

---
+++ DSA/DSA/Tree/BNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Tree
+{
+    public class BNode<T> where T: struct
+    {
+        public BNode<T> Left { get; set; }
+        public BNode<T> Right { get; set; }
+
+        public T Data { get; set; }
+        public BNode(T data,BNode<T> left,BNode<T> right)
+        {
+            Data = data;
+            Left = left;
+            Right = right;
+        }
+        public BNode(T data)
+        {
+            Data = data;
+        }
+    }
+}
